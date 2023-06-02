53c145e9455a9719efac0b079d2a4c5f0f15c7e9
kennykofi <134970756+kennykofi@users.noreply.github.com>
2023-06-02 09:24:46 +0200

    first comment

---
+++ app.js
@@ -0,0 +1,9 @@
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+    console.log('showMenu')
+    navLinks.style.right = "0";
+}
+function hideMenu() {
+    console.log('hideMenu')
+    navLinks.style.right = "-300px";
+}
